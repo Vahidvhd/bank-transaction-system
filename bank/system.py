eb99b3a2b28cacc28c4c2f850cade07d1507ccec
Saman Zhiani <sampayne69@gmail.com>
2026-02-18 18:09:30 +0330

    add initial bank system data structure

---
+++ bank/system.py
@@ -0,0 +1,10 @@
+bank_system = {
+    'accounts': {},
+    'transaction_history': [],
+    'data_file': 'bank_data.pkl',
+    'metadata': {
+        'created_at': '',
+        'last_modified': '',
+        'version': '1.0.0'
+    }
+}
