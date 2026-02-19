9548e130e9d8d3d0a73b54e0a3b2037cfcdc99b3
Saman Zhiani <sampayne69@gmail.com>
2026-02-19 23:42:38 +0330

    feat: add decorator for validating transactions

--- bank/decorators.py
+++ bank/decorators.py
@@ -1,4 +1,30 @@
+from functools import wraps
+from copy import deepcopy
+from datetime import datetime
+import hashlib
+from bank.system import save_system
+
 def validate_transaction(func):
-    def wrapper(*args, **kwargs):
-        return func(*args, **kwargs)
+    @wraps(func)
+    def wrapper(system, *args, **kwargs):
+        snapshot = deepcopy(system)
+
+        try:
+            result = func(system, *args, **kwargs)
+
+            system['transaction_history'].append(transaction_record)
+            save_system(system)
+
+            return result
+
+        except Exception as e:
+            system.clear()
+            system.update(snapshot)
+            raise e
+
     return wrapper
+
+
+
+
+
