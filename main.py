9c11a6cf7c05c646ce03d433739ba106959e05a6
Vahidvhd <vahidvhd91@gmail.com>
2026-02-21 16:53:01 +0000

    feat: add create account flow

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from bank.system import init_system
-from bank.accounts import create_account, transfer
+from bank.system import init_system, save_system
+from bank.accounts import create_account, transfer, gen_acc_id
 
 def demo(system):
     print("Running demo...")
@@ -19,10 +19,49 @@ def demo(system):
 
     print("Demo finished ✅")
 
+def menu():
+    user_menu = input('Select one option:\n1: Log in\n2: Create account').strip()
+    return user_menu
+
+def create_acc(system):
+    print('*** Create account***')
+
+    owner_dict ={
+    'Name' : input('Name: ').strip(),
+    'Fname' : input('Surname: ').strip(),
+    'National id' : input('National id: ').strip(),
+    'Password' : input('Password: ').strip(),
+    'Contact' : {
+        'Phone' : input('Phone Number: ').strip(),
+        'Email' : input('Email: ').strip(),
+        }
+    }
+    while True:
+        init_amout = input('Enter initial deposit amount: ').strip()
+        try:
+            init_amout = float(init_amout)
+            break
+        except ValueError:
+            print('Initial balance cannot contain any letters of special characters')
+            continue
+    
+    account_id = gen_acc_id(system)
+    try:
+        result = create_account(system, account_id, init_amout, owner_dict)
+        save_system(system)
+        print(f"{result['status']}\nAccount number: {result['account_id']}\nBalance: {result['balance']}")
+    except (TypeError , ValueError) as e:
+        print('Error', e)
+
 def main():
     system = init_system()
     print("System loaded/initialized ✅")
-    demo(system)
+    user_choice = menu()
+    if user_choice == '1':
+        demo(system)
+    elif user_choice == '2':
+        create_acc(system)
+
 
 if __name__ == "__main__":
     main()
