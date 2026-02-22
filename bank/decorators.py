cbe88f8db3953bdcee6f5b2d97293ec4f84705db
Saman Zhiani <sampayne69@gmail.com>
2026-02-22 13:21:40 +0330

    make ready to refactor

--- bank/decorators.py
+++ bank/decorators.py
@@ -1,34 +1,37 @@
+from typing import Callable, Dict
+from datetime import datetime
 from functools import wraps
 from copy import deepcopy
-from datetime import datetime
-import hashlib
-from bank.system import save_system
+from .system import save_system
 
-def validate_transaction(func):
+
+def validate_transaction(func: Callable) -> Callable:
+    """Decorator to ensure transactional safety for critical bank operations."""
+
     @wraps(func)
-    def wrapper(system, *args, **kwargs):
-        snapshot = deepcopy(system)
+    def wrapper(system: Dict, *args, **kwargs):
+        # snapshot = deepcopy(system)
 
-        try:
-            result = func(system, *args, **kwargs)
+        # try:
+        #     result = func(system, *args, **kwargs)
+        #     timestamp = datetime.now().isoformat()
+        #     system_string = str(system).encode()
 
-            transaction_record = {
-                # TODO:
-            }
+        #     transaction_record = {
+        #         'args': args,
+        #         'kwargs': kwargs,
+        #         "timestamp": timestamp,
+        #         # TODO:
+        #     }
 
-            system['transaction_history'].append(transaction_record)
-            save_system(system)
+        #     system["transaction_history"].append(transaction_record)
+        #     save_system(system)
 
-            return result
+        #     return result
 
-        except Exception as e:
-            system.clear()
-            system.update(snapshot)
-            raise e
+        # except Exception as e:
+        #     system.clear()
+        #     system.update(snapshot)
+        #     raise e
 
     return wrapper
-
-
-
-
-
