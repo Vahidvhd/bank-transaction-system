f0ac9ec253988823a990fecbdafdbfaefc671bf1
Vahidvhd <vahidvhd91@gmail.com>
2026-02-22 13:14:34 +0000

    feat: implement basic login flow

--- main.py
+++ main.py
@@ -3,50 +3,89 @@ from bank.accounts import create_account, transfer, gen_acc_id
 
 
 def menu():
-    user_menu = input('Select one option:\n1: Log in\n2: Create account\n').strip()
+    user_menu = input('Select one option:\n1: Log in\n2: Create account\n\n\n0: Exit\n>>>: ').strip()
     return user_menu
 
 def create_acc(system):
-    print('*** Create account***')
+    print('***Create account***')
+
+    name = validate_name('Name: ')
+    fname = validate_fname('Surname: ')
+    national_id = validate_national_id('National id: ')
+    password = validate_password('Password: ')
+    phone = validate_phone('Phone: ')
+    email = validate_email('Email: ')
 
     owner_dict ={
-    'Name' : input('Name: ').strip(),
-    'Fname' : input('Surname: ').strip(),
-    'National id' : input('National id: ').strip(),
-    'Password' : input('Password: ').strip(),
-    'Contact' : {
-        'Phone' : input('Phone Number: ').strip(),
-        'Email' : input('Email: ').strip(),
+    'name' : name,
+    'fname' : fname,
+    'national_id' : national_id,
+    'password' : password,
+    'contact' : {
+        'phone' : phone,
+        'email' : email,
         }
     }
+
     while True:
-        init_amout = input('Enter initial deposit amount: ').strip()
+        init_amount = input('Enter initial deposit amount: ').strip()
         try:
-            init_amout = float(init_amout)
+            init_amount = float(init_amount)
             break
         except ValueError:
             print('Initial balance cannot contain any letters of special characters')
             continue
     
     account_id = gen_acc_id(system)
+
     try:
-        result = create_account(system, account_id, init_amout, owner_dict)
+        result = create_account(system, account_id, init_amount, owner_dict)
         save_system(system)
         print(f"{result['status']}\nAccount number: {result['account_id']}\nBalance: {result['balance']}")
     except (TypeError , ValueError) as e:
         print('Error', e)
 
-def main():
-    system = init_system()
-    print("System loaded/initialized ✅")
-    user_choice = menu()
-    if user_choice == '1':
-        print('Login (coming soon)')
-    elif user_choice == '2':
-        create_acc(system)
+def log_in(system):   
+    attemps =0
+    while attemps < 3:
+        acc_id = input('Account Number: ').strip()
+        password = input('Password: ').strip()
+        user_acc = system.get('accounts',{}).get(acc_id)
+        if not user_acc or user_acc['owner']['password'] != password:
+            attemps += 1
+            print(f'Invalid account number or password ({3-attemps} left)')
+        else:
+            print(f"Welcome {user_acc['owner']['Name']}")
+            return acc_id
+
+def log_in_menu(system):
+    print('***Log in***')
+    login_menu = input('1: Log in with Account Number & Password\n2: Forgot Password\n\n\n3: Back\n>>>: ').strip()
+    if login_menu == '1':
+        log_in(system)
+    elif login_menu == '2':
+        # forgot pass
+        pass
+    elif login_menu == '3':
+        return
     else:
         print('Invalid option')
 
+def main():
+    system = init_system()
+    print("System loaded/initialized ✅")
+    while True:
+        user_choice = menu()
+        if user_choice == '1':
+            log_in_menu(system)
+        elif user_choice == '2':
+            create_acc(system)
+        elif user_choice == '0':
+            print('Exit (Not completed yet)')
+            break
+        else:
+            print('Invalid option')
+            continue
 
 if __name__ == "__main__":
     main()
